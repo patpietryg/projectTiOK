93d915ca69938d9b3c950db9ae63a90d5eb2c980
patpietryg <patpietryg@gmail.com>
2023-03-20 16:13:08 +0100

    fixed libraries and built first template

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import requests
-from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-#app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
@@ -17,20 +20,21 @@ def get_posts():
     return response.json()
 
 
-@app.get("/posts/{id}")
-def get_posts(id):
+@app.get("/post/{id}")
+def get_posts(request: Request, id: str):
     url = "https://jsonplaceholder.typicode.com/posts/"+id
     response = requests.get(url)
     #return response.json()
-    return templates.TemplateResponse("post.html", {"request": request, "id": id})
+    return templates.TemplateResponse("post.html", {'request':request, 'context':response.json()})
 
 
 @app.get("/comments")
 def get_posts():
     url = "https://jsonplaceholder.typicode.com/comments"
     response = requests.get(url)
+    print(response.json())
     slownik=list(response.json())[:10]
-    print(slownik[0]["body"])
+    print(slownik[0])
     return response.json()
 
 
