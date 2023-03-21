6e4e631d59578b4182a0280b42402f6a38733be1
patpietryg <patpietryg@gmail.com>
2023-03-21 14:41:09 +0100

    added search form

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+def get_comments_count(post_id):
+    url = f"https://jsonplaceholder.typicode.com/comments?postId={post_id}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        comments = response.json()
+        return len(comments)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching comments for post {post_id}: {e}")
+        return 0
+    except ValueError as e:
+        print(f"Error parsing comments response for post {post_id}: {e}")
+        return 0
+
 @app.get("/")
-async def read_root(request: Request, page: int = 1):
-    response = requests.get(f"https://jsonplaceholder.typicode.com/posts?_page={page}&_limit=10")
+async def read_root(request: Request, page: int = 1, body_length_min: int = None, body_length_max: int = None, search: str = None):
+    url = f"https://jsonplaceholder.typicode.com/posts?_page={page}&_limit=10"
+    if search:
+        url += f"&q={search}"
+    response = requests.get(url)
     posts = response.json()
     total_count = int(response.headers["x-total-count"])
     total_pages = (total_count + 9) // 10
+    for post in posts:
+        post["comments_count"] = get_comments_count(post["id"])
     return templates.TemplateResponse("index.html", {"request": request, "posts": posts, "total_pages": total_pages, "current_page": page})
 
 @app.get("/posts")
